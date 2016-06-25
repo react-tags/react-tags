4b3a2bf1d65d9effb2cc9ca189c1f0d45773803f
Prakhar Srivastav <prakhar1989@gmail.com>
2016-06-25 13:57:21 -0400

    cleaned up tests

--- test/suggestions-test.js
+++ test/suggestions-test.js
@@ -3,54 +3,57 @@ import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
 import Suggestions from '../lib/Suggestions';
 
-describe("Renders suggestions properly", function() {
-  beforeEach(function() {
-    this.component = <Suggestions query={"query"}
-              suggestions={["Banana", "Mango", "Pear", "Apricot"]}
-              selectedIndex={1}
-              handleClick= { (e) =>  null }
-              handleHover= { (e) =>  null }
-              classNames={ { "suggestions": "foo"} }/>
-    this.$el = mount(this.component);
-  });
+function mockItem(overrides) {
+  const defaults = {
+    query: "ang",
+    suggestions: ["Banana", "Mango", "Pear", "Apricot"],
+    selectedIndex: 1,
+    handleClick: (e) => null,
+    handleHover: (e) => null,
+    classNames: { suggestions: "foo" }
+  };
+  const props = Object.assign({}, defaults, overrides);
+  return <Suggestions {...props} />
+}
 
+describe("Renders suggestions properly", function() {
   it("shows the classname properly", function() {
-    expect(this.$el.find('.foo').length).to.equal(1);
+    const $el = shallow(mockItem());
+    expect($el.find('.foo').length).to.equal(1);
   })
 
   it("renders all suggestions properly", function() {
-    expect(this.$el.find('li').length).to.equal(4);
+    const $el = shallow(mockItem());
+    expect($el.find('li').length).to.equal(4);
   })
 
   it("selects the correct suggestion", function() {
-    expect(this.$el.find('li.active').length).to.equal(1);
-    expect(this.$el.find('li.active').text()).to.equal("Mango");
+    const $el = mount(mockItem());
+    expect($el.find('li.active').length).to.equal(1);
+    expect($el.find('li.active').text()).to.equal("Mango");
   });
 
   it("should not render suggestion with less than minQueryLength", function() {
-    let component = <Suggestions query={"q"}
-              suggestions={["Banana", "Mango", "Pear", "Apricot"]}
-              selectedIndex={1}
-              handleClick= { (e) =>  null }
-              handleHover= { (e) =>  null }
-              classNames={ { "suggestions": "foo"} }
-              minQueryLength={2} />
-
-    expect(mount(component).find('.foo').length).to.equal(0);
-    expect(mount(component).find('li').length).to.equal(0);
+    const $el = shallow(mockItem({
+      minQueryLength: 2,
+      query: "q"
+    }));
+    expect($el.find('.foo').length).to.equal(0);
+    expect($el.find('li').length).to.equal(0);
   });
 
-  it("should be able to override suggestion render condiational using a function", function() {
-    let component = <Suggestions query={"ignore_query"}
-              suggestions={["Banana", "Mango", "Pear", "Apricot"]}
-              selectedIndex={1}
-              handleClick={ (e) =>  null }
-              handleHover={ (e) =>  null }
-              shouldRenderSuggestions={ (query) => query !== "ignore_query" }
-              classNames={ { "suggestions": "foo"} }
-              minQueryLength={2} />
-
-    expect(mount(component).find('.foo').length).to.equal(0);
-    expect(mount(component).find('li').length).to.equal(0);
+  it("should be able to override suggestion render", function() {
+    const $el = shallow(mockItem({
+      minQueryLength: 2,
+      query: "ignore_query",
+      shouldRenderSuggestions: (q) => q !== "ignore_query"
+    }));
+    expect($el.find('.foo').length).to.equal(0);
+    expect($el.find('li').length).to.equal(0);
   });
+
+  it("should mark highlighted suggestions correctly", function() {
+    const $el = shallow(mockItem());
+    expect($el.find("li.active").find("span").html()).to.equal("<span>M<mark>ang</mark>o</span>");
+  })
 });
