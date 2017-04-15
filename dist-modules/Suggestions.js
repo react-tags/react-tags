ea44ebb995d07a835784b4edfab3ec478b2c1004
Prakhar Srivastav <prsrivastav@google.com>
2017-04-15 08:39:36 -0700

    fresh build after fixes

--- dist-modules/Suggestions.js
+++ dist-modules/Suggestions.js
@@ -8,7 +8,9 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _lodash = require('lodash');
+var _isEqual = require('lodash/isEqual');
+
+var _isEqual2 = _interopRequireDefault(_isEqual);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -34,7 +36,7 @@ var Suggestions = function (_Component) {
   _inherits(Suggestions, _Component);
 
   function Suggestions() {
-    var _Object$getPrototypeO;
+    var _ref;
 
     var _temp, _this, _ret;
 
@@ -44,12 +46,12 @@ var Suggestions = function (_Component) {
       args[_key] = arguments[_key];
     }
 
-    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_Object$getPrototypeO = Object.getPrototypeOf(Suggestions)).call.apply(_Object$getPrototypeO, [this].concat(args))), _this), _this.shouldComponentUpdate = function (nextProps) {
-      var _this2 = _this;
-      var props = _this2.props;
+    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Suggestions.__proto__ || Object.getPrototypeOf(Suggestions)).call.apply(_ref, [this].concat(args))), _this), _this.shouldComponentUpdate = function (nextProps) {
+      var _this2 = _this,
+          props = _this2.props;
 
       var shouldRenderSuggestions = props.shouldRenderSuggestions || _this.shouldRenderSuggestions;
-      return !(0, _lodash.isEqual)(_this.props.suggestions, nextProps.suggestions) || shouldRenderSuggestions(props.query);
+      return !(0, _isEqual2.default)(_this.props.suggestions, nextProps.suggestions) || shouldRenderSuggestions(props.query);
     }, _this.componentDidUpdate = function (prevProps) {
       var suggestionsContainer = _this.refs.suggestionsContainer;
       if (suggestionsContainer && prevProps.selectedIndex !== _this.props.selectedIndex) {
@@ -65,14 +67,14 @@ var Suggestions = function (_Component) {
         __html: input.replace(RegExp(escapedRegex, "gi"), "<mark>$&</mark>")
       };
     }, _this.shouldRenderSuggestions = function (query) {
-      var _this3 = _this;
-      var props = _this3.props;
+      var _this3 = _this,
+          props = _this3.props;
 
       var minQueryLength = props.minQueryLength || 2;
       return props.query.length >= minQueryLength;
     }, _this.render = function () {
-      var _this4 = _this;
-      var props = _this4.props;
+      var _this4 = _this,
+          props = _this4.props;
 
       var suggestions = props.suggestions.map(function (item, i) {
         return _react2.default.createElement(
--- dist-modules/reactTags.js
+++ dist-modules/reactTags.js
@@ -69,7 +69,10 @@ var ReactTags = _react2.default.createClass({
     removeComponent: _react2.default.PropTypes.func,
     autocomplete: _react2.default.PropTypes.oneOfType([_react2.default.PropTypes.bool, _react2.default.PropTypes.number]),
     readOnly: _react2.default.PropTypes.bool,
-    classNames: _react2.default.PropTypes.object
+    classNames: _react2.default.PropTypes.object,
+    name: _react2.default.PropTypes.string,
+    id: _react2.default.PropTypes.string,
+    maxLength: _react2.default.PropTypes.string
   },
   getDefaultProps: function getDefaultProps() {
     return {
@@ -150,10 +153,10 @@ var ReactTags = _react2.default.createClass({
     }
   },
   handleKeyDown: function handleKeyDown(e) {
-    var _state = this.state;
-    var query = _state.query;
-    var selectedIndex = _state.selectedIndex;
-    var suggestions = _state.suggestions;
+    var _state = this.state,
+        query = _state.query,
+        selectedIndex = _state.selectedIndex,
+        suggestions = _state.suggestions;
 
     // hide suggestions menu on escape
 
@@ -308,7 +311,10 @@ var ReactTags = _react2.default.createClass({
     var query = this.state.query.trim(),
         selectedIndex = this.state.selectedIndex,
         suggestions = this.state.suggestions,
-        placeholder = this.props.placeholder;
+        placeholder = this.props.placeholder,
+        inputName = this.props.name,
+        inputId = this.props.id,
+        maxLength = this.props.maxLength;
 
     var tagInput = !this.props.readOnly ? _react2.default.createElement(
       'div',
@@ -321,7 +327,10 @@ var ReactTags = _react2.default.createClass({
         onBlur: this.handleBlur,
         onChange: this.handleChange,
         onKeyDown: this.handleKeyDown,
-        onPaste: this.handlePaste }),
+        onPaste: this.handlePaste,
+        name: inputName,
+        id: inputId,
+        maxLength: maxLength }),
       _react2.default.createElement(_Suggestions2.default, { query: query,
         suggestions: suggestions,
         selectedIndex: selectedIndex,
