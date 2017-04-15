8d854122fe9cb6ccd375e5b9e4dc6812b69be94a
Aristotelis Dossas <teldosas@gmail.com>
2017-04-15 22:05:47 +0300

    Add webpack configuration that works

---
+++ webpack-dev-server.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const path = require('path');
+const buildPath = path.resolve(__dirname, 'build');
+const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+
+const config = {
+  // Entry points to the project
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, '/lib/ReactTags.js'),
+  ],
+  // Server Configuration options
+  devServer: {
+    contentBase: '.', // Relative directory for base of server
+    devtool: 'cheap-module-source-map',
+    hot: true, // Live-reload
+    inline: true,
+    port: 8090, // Port Number
+    host: 'localhost', // Change to '0.0.0.0' for external facing server
+  },
+  devtool: 'cheap-module-source-map',
+  output: {
+    path: buildPath, // Path of output file
+    filename: '/dist/ReactTags.min.js',
+    libraryTarge: 'umd',
+    library: 'ReactTags'
+  },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+    'react-dnd': 'ReactDnD'
+  },
+  plugins: [
+    // Enables Hot Modules Replacement
+    new webpack.HotModuleReplacementPlugin(),
+    // Allows error warnings but does not stop compiling.
+    new webpack.NoErrorsPlugin(),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, // All .js files
+        loaders: ['babel-loader'],
+        exclude: [nodeModulesPath],
+      },
+    ],
+  },
+};
+
+module.exports = config;
