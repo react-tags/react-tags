83e442c7e1da9ac6c51ce0f241dfd6cd98c45823
Prakhar Srivastav <prsrivastav@google.com>
2017-04-16 21:15:41 -0700

    updated webpack configs after #161

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -1,47 +1,45 @@
-const webpack = require('webpack');
-const path = require('path');
-const buildPath = path.resolve(__dirname, 'build');
-const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+const webpack = require("webpack");
+const path = require("path");
+const buildPath = path.resolve(__dirname, "dist");
+const nodeModulesPath = path.resolve(__dirname, "node_modules");
 
 const config = {
   // Entry points to the project
   entry: [
-    'webpack/hot/dev-server',
-    'webpack/hot/only-dev-server',
-    path.join(__dirname, '/lib/ReactTags.js'),
+    "webpack/hot/only-dev-server",
+    path.join(__dirname, "/lib/ReactTags.js"),
   ],
   // Server Configuration options
   devServer: {
-    contentBase: '.', // Relative directory for base of server
-    devtool: 'cheap-module-source-map',
+    contentBase: ".", // Relative directory for base of server
     hot: true, // Live-reload
     inline: true,
     port: 8090, // Port Number
-    host: 'localhost', // Change to '0.0.0.0' for external facing server
+    host: "localhost", // Change to '0.0.0.0' for external facing server
   },
-  devtool: 'cheap-module-source-map',
+  devtool: "cheap-module-source-map",
   output: {
     path: buildPath, // Path of output file
-    filename: '/dist/ReactTags.min.js',
-    libraryTarge: 'umd',
-    library: 'ReactTags'
+    filename: "ReactTags.min.js",
+    libraryTarget: "umd",
+    library: "ReactTags",
   },
   externals: {
-    'react': 'React',
-    'react-dom': 'ReactDOM',
-    'react-dnd': 'ReactDnD'
+    react: "React",
+    "react-dom": "ReactDOM",
+    "react-dnd": "ReactDnD",
   },
   plugins: [
     // Enables Hot Modules Replacement
     new webpack.HotModuleReplacementPlugin(),
     // Allows error warnings but does not stop compiling.
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
   ],
   module: {
     loaders: [
       {
         test: /\.js$/, // All .js files
-        loaders: ['babel-loader'],
+        loaders: ["babel-loader"],
         exclude: [nodeModulesPath],
       },
     ],
--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,30 +1,30 @@
-const webpack = require('webpack');
-const path = require('path');
-const buildPath = path.resolve(__dirname, 'dist');
-const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+const webpack = require("webpack");
+const path = require("path");
+const buildPath = path.resolve(__dirname, "dist");
+const nodeModulesPath = path.resolve(__dirname, "node_modules");
 
 const config = {
-  entry: [path.join(__dirname, '/lib/ReactTags.js')],
+  entry: [path.join(__dirname, "/lib/ReactTags.js")],
   // Render source-map file for final build
-  devtool: 'source-map',
+  devtool: "source-map",
   // output config
   output: {
     path: buildPath, // Path of output file
-    filename: 'ReactTags.min.js', // Name of output file
-    libraryTarge: 'umd',
-    library: 'ReactTags'
+    filename: "ReactTags.min.js", // Name of output file
+    libraryTarget: "umd",
+    library: "ReactTags",
   },
   externals: {
-    'react': 'React',
-    'react-dom': 'ReactDOM',
-    'react-dnd': 'ReactDnD'
+    react: "React",
+    "react-dom": "ReactDOM",
+    "react-dnd": "ReactDnD",
   },
   plugins: [
     // Define production build to allow React to strip out unnecessary checks
     new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('production')
-      }
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+      },
     }),
     // Minify the bundle
     new webpack.optimize.UglifyJsPlugin({
@@ -34,13 +34,13 @@ const config = {
       },
     }),
     // Allows error warnings but does not stop compiling.
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
   ],
   module: {
     loaders: [
       {
         test: /\.js$/, // All .js files
-        loaders: ['babel-loader'], // react-hot is like browser sync and babel loads jsx and es6-7
+        loaders: ["babel-loader"], // react-hot is like browser sync and babel loads jsx and es6-7
         exclude: [nodeModulesPath],
       },
     ],
