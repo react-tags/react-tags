418cacdbc38d1456957be700f22954313b003bb7
Aakansha Doshi <aakansha1216@gmail.com>
2018-03-23 00:10:21 +0530

    fix prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  printWidth: 80,
+  proseWrap: 'always',
+  'tab-width': 2,
+  'use-tabs': false,
+  trailingComma: 'es5',
+  jsxBracketSameLine: true,
+  arrowParens: 'always',
+  parser: 'flow',
+};
