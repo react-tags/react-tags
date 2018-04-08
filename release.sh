b29c5e1202bd81ffa5f8353eac652b8e9691b263
Prakhar Srivastav <prakhar1989@gmail.com>
2018-04-08 09:23:41 -0700

    added basic checks in release.sh

--- release.sh
+++ release.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
-echo "Version $1.$2.$3 in progress"
-git checkout master
-git pull -r origin master
-npm build
-git tag v$1.$2.$3
-git push origin --tags
-git commit -am "upgrading to version $1.$2.$3"
-git push origin master
-npm publish
-echo "Version published successfully"
+
+# releases a new version
+release () {
+  echo "Version $1 in progress"
+  git checkout master
+  git pull -r origin master
+  npm build
+  git tag v$1
+  git push origin --tags
+  git commit -am "upgrading to version $1"
+  git push origin master
+  npm publish
+  echo "Version published successfully"
+}
+
+if [ $# -ne 3 ]
+  then
+  echo "Usage: ./release.sh 3 2 1 for releasing v3.2.1"
+  exit 1
+fi
+echo "This will release new Version v$1.$2.$3...."
+
+# ask for user's confirmation
+read -p "Are you sure you want to continue? (y/n) " choice
+case "$choice" in
+  y|Y) release "$1.$2.$3" ;;
+  *) echo "Aborting..."
+esac
