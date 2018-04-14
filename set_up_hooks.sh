523082d604ecf441c9aa4a3ae8afb80237c1905f
Aakansha Doshi <aakansha1216@gmail.com>
2018-04-14 16:26:49 +0530

    Add a script to create precommit hook

---
+++ set_up_hooks.sh
@@ -0,0 +1,6 @@
+mkdir -p .git/hooks
+touch .git/hooks/pre-commit
+cp .hooks/pre-commit .git/hooks/pre-commit
+chmod a+x .git/hooks/pre-commit
+
+printf "\033[42mPrecommit hook created successfully\033[0m"
