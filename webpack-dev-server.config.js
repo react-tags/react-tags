dd270db695547a02c84eedb0217b052f38cfac27
Trond Arne Bråthen <tabrath@gmail.com>
2018-06-07 19:55:06 +0200

    Upgrade webpack to v4.11.1 (#341)
    * chore: upgrade webpack to v4.11.1
    
    * Also upgraded webpack-dev-server and babel-loader and the cli tools are split out to webpack-cli, so installed that.
    * updated configuration for new syntax. UglifyJS is now replaced by optimization.minimize flag.

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -1,9 +1,9 @@
 const webpack = require("webpack");
 const path = require("path");
 const buildPath = path.resolve(__dirname, "dist");
-const nodeModulesPath = path.resolve(__dirname, "node_modules");
 
 const config = {
+  mode: 'development',
   // Entry points to the project
   entry: [
     "webpack/hot/only-dev-server",
@@ -36,11 +36,13 @@ const config = {
     new webpack.NoEmitOnErrorsPlugin(),
   ],
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/, // All .js files
-        loaders: ["babel-loader"],
-        exclude: [nodeModulesPath],
+        use: [{ // react-hot is like browser sync and babel loads jsx and es6-7
+          loader: 'babel-loader',
+        }],
+        exclude: /node_modules/,
       },
     ],
   },
--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,9 +1,9 @@
 const webpack = require("webpack");
 const path = require("path");
 const buildPath = path.resolve(__dirname, "dist");
-const nodeModulesPath = path.resolve(__dirname, "node_modules");
 
 const config = {
+  mode: 'production',
   entry: [path.join(__dirname, "/lib/ReactTags.js")],
   // Render source-map file for final build
   devtool: "source-map",
@@ -26,24 +26,22 @@ const config = {
         NODE_ENV: JSON.stringify("production"),
       },
     }),
-    // Minify the bundle
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        // suppresses warnings, usually from module minification
-        warnings: false,
-      },
-    }),
     // Allows error warnings but does not stop compiling.
     new webpack.NoEmitOnErrorsPlugin(),
   ],
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/, // All .js files
-        loaders: ["babel-loader"], // react-hot is like browser sync and babel loads jsx and es6-7
-        exclude: [nodeModulesPath],
+        use: [{ // react-hot is like browser sync and babel loads jsx and es6-7
+          loader: 'babel-loader',
+        }],
+        exclude: /node_modules/,
       },
     ],
+  },
+  optimization: {
+    minimize: true,
   },
 };
 
