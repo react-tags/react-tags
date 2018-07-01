1d772fac32629f90964be404fd688a6ab41e2fbf
Aakansha Doshi <aakansha1216@gmail.com>
2018-07-01 19:04:59 +0530

    refactor: release script

--- release.sh
+++ release.sh
@@ -8,10 +8,12 @@ release () {
   git tag v$1 &&
   git push origin --tags &&
   npm run build &&
-  git commit -am "upgrading to version $1" &&
+  npm run format &&
+  git add .
+  git commit -nm "version bump $1.$2.$3" &&
   git push origin master &&
   npm publish
-  echo "Version published successfully"
+  echo "Version $1 published successfully"
 }
 
 if [ $# -ne 3 ]
@@ -19,7 +21,7 @@ if [ $# -ne 3 ]
   echo "Usage: ./release.sh 3 2 1 for releasing v3.2.1"
   exit 1
 fi
-echo "This will release new Version v$1.$2.$3...."
+echo "This will release new Version v$1.$2.$3"
 
 # ask for user's confirmation
 read -p "Are you sure you want to continue? (y/n) " choice
