89b4b25af07805a7c72c423b1fd7783912747eea
Aakansha Doshi <aakansha1216@gmail.com>
2021-07-23 21:46:15 +0530

    docs: update docs to use hooks

--- example/main.js
+++ example/main.js
@@ -226,71 +226,53 @@ const delimiters = [...KeyCodes.enter, KeyCodes.comma];
 
 const Tags = ReactTags.WithContext;
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [tags, setTags] = React.useState([
+    { id: 'Thailand', text: 'Thailand' },
+    { id: 'India', text: 'India' },
+    { id: 'Vietnam', text: 'Vietnam' },
+    { id: 'Turkey', text: 'Turkey' },
+  ]);
 
-    this.state = {
-      tags: [
-        { id: 'Thailand', text: 'Thailand' },
-        { id: 'India', text: 'India' },
-        { id: 'Vietnam', text: 'Vietnam' },
-        { id: 'Turkey', text: 'Turkey' },
-      ],
-      suggestions: suggestions,
-    };
-    this.handleDelete = this.handleDelete.bind(this);
-    this.handleAddition = this.handleAddition.bind(this);
-    this.handleDrag = this.handleDrag.bind(this);
-    this.handleTagClick = this.handleTagClick.bind(this);
-  }
+  const handleDelete = (i) => {
+    setTags(tags.filter((tag, index) => index !== i));
+  };
 
-  handleDelete(i) {
-    const { tags } = this.state;
-    this.setState({
-      tags: tags.filter((tag, index) => index !== i),
-    });
-  }
+  const handleAddition = (tag) => {
+    setTags([...tags, tag]);
+  };
 
-  handleAddition(tag) {
-    this.setState((state) => ({ tags: [...state.tags, tag] }));
-  }
-
-  handleDrag(tag, currPos, newPos) {
-    const tags = [...this.state.tags];
+  const handleDrag = (tag, currPos, newPos) => {
     const newTags = tags.slice();
 
     newTags.splice(currPos, 1);
     newTags.splice(newPos, 0, tag);
 
     // re-render
-    this.setState({ tags: newTags });
-  }
+    setTags(newTags);
+  };
 
-  handleTagClick(index) {
+  const handleTagClick = (index) => {
     console.log('The tag at index ' + index + ' was clicked');
-  }
-
-  render() {
-    const { tags, suggestions } = this.state;
-    return (
-      <div className="app">
-        <h1> React Tags Example </h1>
-        <div>
-          <Tags
-            tags={tags}
-            suggestions={suggestions}
-            delimiters={delimiters}
-            handleDelete={this.handleDelete}
-            handleAddition={this.handleAddition}
-            handleDrag={this.handleDrag}
-            handleTagClick={this.handleTagClick}
-            inputFieldPosition="bottom"
-            autocomplete
-          />
-        </div>
+  };
+  return (
+    <div className="app">
+      <h1> React Tags Example </h1>
+      <div>
+        <Tags
+          tags={tags}
+          suggestions={suggestions}
+          delimiters={delimiters}
+          handleDelete={handleDelete}
+          handleAddition={handleAddition}
+          handleDrag={handleDrag}
+          handleTagClick={handleTagClick}
+          inputFieldPosition="bottom"
+          autocomplete
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 ReactDOM.render(<App />, document.getElementById('app'));
