28074febaee52722ec09e753aef6574e21885beb
mrdannael <mr.dannael@gmail.com>
2023-04-02 17:53:53 +0200

    Configure vite build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import dts from 'vite-plugin-dts'
+import * as packageJson from "./package.json"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    dts({
+      include: ['src/']
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve('src', 'index.tsx'),
+      name: 'react-tag-input',
+      formats: ['es', 'umd'],
+      fileName: (format) => `react-tag-input.${format}.js`
+    },
+    rollupOptions: {
+      external: [...Object.keys([packageJson.peerDependencies])],
+    },
+  },
 })
