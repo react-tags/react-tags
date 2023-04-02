9eb59740a0b41400217515c0459cb26e661859fa
mrdannael <mr.dannael@gmail.com>
2023-04-02 17:33:11 +0200

    Configure eslint, prettier, husky and lint-staged

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "prettier",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:jsx-a11y/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: [
+    "react",
+    "import",
+    "jsx-a11y",
+    "@typescript-eslint",
+    "react-hooks",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    indent: ["error", 2, { "SwitchCase": 1 }],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "linebreak-style": ["error", "unix"],
+  }
+};
--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       React Tags
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
