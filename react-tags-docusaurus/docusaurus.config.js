61425883369408c318ddde01a7141770c46f92ff
Syed Hisham Akmal <hisham0502@gmail.com>
2023-10-21 02:42:09 +0530

    Changed title of docusaurus docs website

--- react-tags-docusaurus/docusaurus.config.js
+++ react-tags-docusaurus/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Docs',
+  title: 'React Tags Docs',
   favicon: 'img/favicon2.ico',
 
   // Set the production url of your site here
