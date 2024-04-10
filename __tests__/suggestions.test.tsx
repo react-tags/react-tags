5155dcbf19a142917a8037f7b786832c1c314875
Aakansha Doshi <aakansha1216@gmail.com>
2024-04-10 15:38:29 +0530

    Fix and add more tests

--- __tests__/suggestions.test.tsx
+++ __tests__/suggestions.test.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { expect } from 'chai';
 import { shallow, mount } from 'enzyme';
 import { spy, stub } from 'sinon';
-import Suggestions from '../src/components/Suggestions';
+import Suggestions, { arePropsEqual } from '../src/components/Suggestions';
 import noop from 'lodash/noop';
 import { DEFAULT_LABEL_FIELD } from '../src/components/constants';
 
@@ -23,39 +23,35 @@ const defaults = {
   classNames: { suggestions: 'foo', activeSuggestion: 'active' },
 };
 
-function mockItem(overrides) {
+const mockItem = (overrides?: any) => {
   const props = Object.assign({}, defaults, overrides);
   return <Suggestions {...props} />;
-}
+};
 
-describe('Suggestions', function() {
-  test('should render with expected props', function() {
-    const $el = shallow(mockItem());
-    const expectedProps = {
-      minQueryLength: 2,
-      ...defaults,
-    };
+describe('Suggestions', () => {
+  it('should render with expected props', () => {
+    const $el = mount(mockItem());
 
-    expect($el.instance().props).deep.equal(expectedProps);
+    expect($el.props()).deep.equal(defaults);
   });
 
-  test('shows the classname properly', function() {
+  it('shows the classname properly', () => {
     const $el = shallow(mockItem());
     expect($el.find('.foo').length).to.equal(1);
   });
 
-  test('renders all suggestions properly', function() {
+  it('renders all suggestions properly', () => {
     const $el = shallow(mockItem());
     expect($el.find('li').length).to.equal(4);
   });
 
-  test('selects the correct suggestion', function() {
+  it('selects the correct suggestion', () => {
     const $el = mount(mockItem());
     expect($el.find('li.active').length).to.equal(1);
     expect($el.find('li.active').text()).to.equal('Mango');
   });
 
-  test('should not render suggestion with less than minQueryLength', function() {
+  it('should not render suggestion with less than minQueryLength', () => {
     const $el = shallow(
       mockItem({
         minQueryLength: 2,
@@ -66,7 +62,7 @@ describe('Suggestions', function() {
     expect($el.find('li').length).to.equal(0);
   });
 
-  test('should be able to override suggestion render', function() {
+  it('should be able to override suggestion render', () => {
     const $el = shallow(
       mockItem({
         minQueryLength: 2,
@@ -78,7 +74,7 @@ describe('Suggestions', function() {
     expect($el.find('li').length).to.equal(0);
   });
 
-  test('should escape html characters in query', function() {
+  it('should escape html characters in query', () => {
     const suggestions = [{ id: 'script', text: '<script>alert()</script>' }];
     const $el = shallow(
       mockItem({
@@ -87,127 +83,121 @@ describe('Suggestions', function() {
       })
     );
     expect($el.html()).to.equal(
-      '<div class="foo"><ul> <li class=""><span><mark>&lt;script&gt;alert()&lt;/script&gt;</mark></span></li> </ul></div>'
+      '<div class="foo"><ul> ' +
+        '<li class=""><span><mark>&lt;script&gt;alert()&lt;/script&gt;</mark></span></li> ' +
+        '</ul></div>'
     );
   });
 
-  test('should mark highlighted suggestions correctly', function() {
+  it('should mark highlighted suggestions correctly', () => {
     const $el = shallow(mockItem());
-    expect(
-      $el
-        .find('li.active')
-        .find('span')
-        .html()
-    ).to.equal('<span>M<mark>ang</mark>o</span>');
-  });
-
-  test('should not wastefully re-render if the list of suggestions have not changed', function() {
-    const suggestions = [
-      { id: 'queue', text: 'queue' },
-      { id: 'quiz', text: 'quiz' },
-      { id: 'quantify', text: 'quantify' },
-    ];
-    const $el = mount(
-      mockItem({
-        minQueryLength: 2,
-        query: 'q',
-        suggestions,
-      })
+    expect($el.find('li.active').find('span').html()).to.equal(
+      '<span>M<mark>ang</mark>o</span>'
     );
-
-    spy(Suggestions.prototype, 'componentDidUpdate');
-    $el.setProps({ suggestions });
-    expect(Suggestions.prototype.componentDidUpdate.called).to.equal(false);
-    Suggestions.prototype.componentDidUpdate.restore();
   });
 
-  test('should re-render if there is an active query', function() {
-    const suggestions = [
-      { id: 'queue', text: 'queue' },
-      { id: 'quiz', text: 'quiz' },
-      { id: 'quantify', text: 'quantify' },
-    ];
-    const $el = mount(
-      mockItem({
-        minQueryLength: 2,
-        query: 'qu',
-        suggestions,
-      })
-    );
-    spy(Suggestions.prototype, 'componentDidUpdate');
-    $el.setProps({ suggestions });
-    expect(Suggestions.prototype.componentDidUpdate.called).to.equal(true);
-    Suggestions.prototype.componentDidUpdate.restore();
-  });
+  // it('should not wastefully re-render if the list of suggestions have not changed', () => {
+  //   const suggestions = [
+  //     { id: 'queue', text: 'queue' },
+  //     { id: 'quiz', text: 'quiz' },
+  //     { id: 'quantify', text: 'quantify' },
+  //   ];
+  //   const $el = mount(
+  //     mockItem({
+  //       minQueryLength: 2,
+  //       query: 'q',
+  //       suggestions,
+  //     })
+  //   );
 
-  test('should re-render if minQueryLength is set to 0', function() {
-    const suggestions = [
-      { id: 'queue', text: 'queue' },
-      { id: 'quiz', text: 'quiz' },
-      { id: 'quantify', text: 'quantify' },
-    ];
-    const $el = mount(
-      mockItem({
-        minQueryLength: 0,
-        query: '',
-        suggestions,
-      })
-    );
-    spy(Suggestions.prototype, 'componentDidUpdate');
-    $el.setProps({ suggestions });
-    expect(Suggestions.prototype.componentDidUpdate.called).to.equal(true);
-    Suggestions.prototype.componentDidUpdate.restore();
-  });
+  //   $el.setProps({ suggestions });
+  // });
 
-  test('should re-render if the provided "shouldRenderSuggestions" prop returns true', function() {
-    const suggestions = [
-      { id: 'queue', text: 'queue' },
-      { id: 'quiz', text: 'quiz' },
-      { id: 'quantify', text: 'quantify' },
-    ];
-    const $el = mount(
-      mockItem({
-        shouldRenderSuggestions: function() {
-          return true;
-        },
-        suggestions,
-      })
-    );
-    spy(Suggestions.prototype, 'componentDidUpdate');
-    $el.setProps({ suggestions });
-    expect(Suggestions.prototype.componentDidUpdate.called).to.equal(true);
-    Suggestions.prototype.componentDidUpdate.restore();
-  });
+  // it('should re-render if there is an active query', () => {
+  //   const suggestions = [
+  //     { id: 'queue', text: 'queue' },
+  //     { id: 'quiz', text: 'quiz' },
+  //     { id: 'quantify', text: 'quantify' },
+  //   ];
+  //   const $el = mount(
+  //     mockItem({
+  //       minQueryLength: 2,
+  //       query: 'qu',
+  //       suggestions,
+  //     })
+  //   );
+  //   spy(Suggestions.prototype, 'componentDidUpdate');
+  //   $el.setProps({ suggestions });
+  //   expect(Suggestions.prototype.componentDidUpdate.called).to.equal(true);
+  //   Suggestions.prototype.componentDidUpdate.restore();
+  // });
 
-  test('should re-render when the query reaches minQueryLength', function() {
-    const suggestions = [
-      { id: 'queue', text: 'queue' },
-      { id: 'quiz', text: 'quiz' },
-      { id: 'quantify', text: 'quantify' },
-    ];
-    let div = document.createElement('div');
-    let component = mockItem({
-      minQueryLength: 2,
-      query: '',
-      suggestions,
-    });
-    var $el = ReactDOM.render(component, div);
-    spy($el, 'componentDidUpdate');
+  // it('should re-render if minQueryLength is set to 0', () => {
+  //   const suggestions = [
+  //     { id: 'queue', text: 'queue' },
+  //     { id: 'quiz', text: 'quiz' },
+  //     { id: 'quantify', text: 'quantify' },
+  //   ];
+  //   const $el = mount(
+  //     mockItem({
+  //       minQueryLength: 0,
+  //       query: '',
+  //       suggestions,
+  //     })
+  //   );
+  //   spy(Suggestions.prototype, 'componentDidUpdate');
+  //   $el.setProps({ suggestions });
+  //   expect(Suggestions.prototype.componentDidUpdate.called).to.equal(true);
+  //   Suggestions.prototype.componentDidUpdate.restore();
+  // });
 
-    // re-render with updated query prop
-    $el = ReactDOM.render(
-      mockItem({
-        minQueryLength: 2,
-        query: 'qu',
-        suggestions,
-      }),
-      div
-    );
+  // it('should re-render if the provided "shouldRenderSuggestions" prop returns true', () => {
+  //   const suggestions = [
+  //     { id: 'queue', text: 'queue' },
+  //     { id: 'quiz', text: 'quiz' },
+  //     { id: 'quantify', text: 'quantify' },
+  //   ];
+  //   const $el = mount(
+  //     mockItem({
+  //       shouldRenderSuggestions: () => true,
+  //       suggestions,
+  //     })
+  //   );
+  //   spy(Suggestions.prototype, 'componentDidUpdate');
+  //   $el.setProps({ suggestions });
+  //   expect(Suggestions.prototype.componentDidUpdate.called).to.equal(true);
+  //   Suggestions.prototype.componentDidUpdate.restore();
+  // });
 
-    expect($el.componentDidUpdate.called).to.equal(true);
-  });
+  // it('should re-render when the query reaches minQueryLength', () => {
+  //   const suggestions = [
+  //     { id: 'queue', text: 'queue' },
+  //     { id: 'quiz', text: 'quiz' },
+  //     { id: 'quantify', text: 'quantify' },
+  //   ];
+  //   let div = document.createElement('div');
+  //   let component = mockItem({
+  //     minQueryLength: 2,
+  //     query: '',
+  //     suggestions,
+  //   });
+  //   var $el = ReactDOM.render(component, div);
+  //   spy($el, 'componentDidUpdate');
 
-  test('should render custom suggestions when renderSuggestion prop is provided', function() {
+  //   // re-render with updated query prop
+  //   $el = ReactDOM.render(
+  //     mockItem({
+  //       minQueryLength: 2,
+  //       query: 'qu',
+  //       suggestions,
+  //     }),
+  //     div
+  //   );
+
+  //   expect($el.componentDidUpdate.called).to.equal(true);
+  // });
+
+  it('should render custom suggestions when renderSuggestion prop is provided', () => {
     const $el = shallow(
       mockItem({
         renderSuggestion: ({ text }) => (
@@ -222,13 +212,65 @@ describe('Suggestions', function() {
     expect($el.find('.bar').length).to.equal(4);
   });
 
-  test('should trigger the click handler on touchStart', function() {
+  it('should trigger the click handler on touchStart', () => {
     const onTagClickedStub = stub();
     const $el = mount(mockItem({ handleClick: onTagClickedStub }));
-    $el
-      .find('li')
-      .first()
-      .simulate('touchStart');
+    $el.find('li').first().simulate('touchStart');
     expect(onTagClickedStub.calledOnce).to.be.true;
+  });
+
+  describe('Test arePropsEqual', () => {
+    const prevProps = {
+      query: 'ang',
+      suggestions: [
+        { id: 'Banana', text: 'Banana' },
+        { id: 'Mango', text: 'Mango' },
+        { id: 'Pear', text: 'Pear' },
+        { id: 'Apricot', text: 'Apricot' },
+      ],
+      labelField: 'text',
+      selectedIndex: 1,
+      isFocused: true,
+      handleClick: noop,
+      handleHover: noop,
+      classNames: { suggestions: 'foo', activeSuggestion: 'active' },
+    };
+    const nextProps = { ...prevProps };
+
+    it('should return false if isFocussed prop changes', () => {
+      expect(
+        arePropsEqual(prevProps, { ...nextProps, isFocused: false })
+      ).to.equal(false);
+    });
+
+    it('should return false if suggestions prop changes', () => {
+      expect(
+        arePropsEqual(prevProps, {
+          ...nextProps,
+          suggestions: [
+            { id: 'Banana', text: 'Banana' },
+            { id: 'Mango', text: 'Mango' },
+            { id: 'Pear', text: 'Pear' },
+          ],
+        })
+      ).to.equal(false);
+    });
+
+    it('should return false if minQueryLength prop changes leading to no suggestions', () => {
+      expect(
+        arePropsEqual(prevProps, {
+          ...nextProps,
+          // Will lead to no suggestions as shouldRenderSuggestions returns false since minQueryLength greater than query length (3)
+          minQueryLength: 4,
+        })
+      ).to.equal(false);
+    });
+
+    it('should return true only if isFocused, suggestions, minQueryLength props and return value of shouldRenderSuggestions are the same', () => {
+      // Even if label field is getting updated in next Props still the function will return true
+      expect(
+        arePropsEqual(prevProps, { ...nextProps, labelField: 'name' })
+      ).to.equal(true);
+    });
   });
 });
